b7dc00f2b062be728fbb55a02c408f65176e723e
Tuan Chuong Goh <chuong.goh@arm.com>
2023-11-20 15:10:25 +0000

    Exercise 2: Data Types

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <cstdint>
+#include <string>
+
+enum class houseCode : std::uint8_t {
+    INVALID, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O
+};
+
+enum class State {On, Off};
+
+const char* HouseName = "~ABCDEFGHIJKLMNO";
+
+struct Device{
+    houseCode houseCode;
+    short unitCode;
+};
+
+struct Lamp {
+    Device deviceID;
+    bool lampState;
+};
 
 int main(){
-    std::cout << "std::sizeof(short): " << sizeof(short) << std::endl;
-    std::cout << "std::sizeof(int): " << sizeof(int) << std::endl;
-    std::cout << "std::sizeof(long): " << sizeof(long) << std::endl;
-    std::cout << "std::sizeof(long long): " << sizeof(long long) << std::endl;
-    std::cout << "std::sizeof(void*): " << sizeof(void*) << std::endl;
-    return 0;
+    
+    Lamp lamp;
+    lamp.deviceID = Device {houseCode::A, 10};
+    lamp.lampState = false;
+
+    std::cout << "Lamp House Code: " << HouseName[static_cast<uint8_t>(lamp.deviceID.houseCode)] << "\n";
+    std::cout << "Lamp Unit Code: " << std::to_string(lamp.deviceID.unitCode) << "\n";
+    std::cout << "Lamp State: " << (lamp.lampState? "On" : "Off") << "\n";
 }
