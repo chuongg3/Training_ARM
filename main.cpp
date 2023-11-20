3c16b526b4abfd022506000fd0e1e63779c92328
Tuan Chuong Goh <chuong.goh@arm.com>
2023-11-20 15:35:11 +0000

    Exercise 3: Arrays

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdint>
 #include <string>
+#include <array>
 
 enum class houseCode : std::uint8_t {
     INVALID, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O
@@ -26,7 +27,18 @@ int main(){
     lamp.deviceID = Device {houseCode::A, 10};
     lamp.lampState = false;
 
-    std::cout << "Lamp House Code: " << HouseName[static_cast<uint8_t>(lamp.deviceID.houseCode)] << "\n";
-    std::cout << "Lamp Unit Code: " << std::to_string(lamp.deviceID.unitCode) << "\n";
-    std::cout << "Lamp State: " << (lamp.lampState? "On" : "Off") << "\n";
+    std::array<Lamp, 10> lamps{
+        Lamp {{houseCode::A, 1}, false},
+        Lamp {{houseCode::B, 2}, false},
+        Lamp {{houseCode::C, 3}, false},
+        Lamp {{houseCode::D, 4}, false},
+        Lamp {{houseCode::E, 5}, false},
+    };
+
+    std::cout << "Size of array: " << lamps.size() << "\n";
+    for (auto iterator = std::begin(lamps); iterator != std::end(lamps); iterator++){
+        if (iterator->deviceID.houseCode != houseCode::INVALID){
+            std::cout << "Current Lamp's Unit Code: " << iterator->deviceID.unitCode << "\n";
+        }
+    }
 }
